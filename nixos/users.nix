35ea14f8383e01b0b8c8408d6cd4115b9ebaa631
Alejandro Suarez <ing.alsuga@gmail.com>
2025-08-02 11:57:23 -0500

    feat(nixos): allowing desktop public key

--- nixos/users.nix
+++ nixos/users.nix
@@ -13,6 +13,7 @@
     hashedPasswordFile = config.age.secrets.user-password.path;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOtOvtzUhnmekCdm+VaF6lGbxiIHZ4qmcecvqnOzVcew ing.alsuga@gmail.com"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG2aNQQRfJsIrWRtFyntXyxWgsdw9J1VHyZNLv2DQIwb ing.alsuga@gmail.com"
     ];
   };
 }
